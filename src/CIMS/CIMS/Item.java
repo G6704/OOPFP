c8b0c67a70f354faee9951eda398aeec59b202c8
G6704 <98708687+G6704@users.noreply.github.com>
2023-06-12 07:12:57 +0700

    Delete src/CIMS directory

--- src/CIMS/CIMS/Item.java
+++
@@ -1,6 +0,0 @@
-package CIMS;
-//This interface for the clothing item. The importance of the interface is to allow the better planning of methods
-public interface Item {
-    public String getItemId();
-    public void setQuantity(int quantity);
-}
